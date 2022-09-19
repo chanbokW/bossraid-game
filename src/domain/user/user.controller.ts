6231a4305db04725b67347da68817b180d8df09f
chanbokW <cksqhr4961@naver.com>
2022-09-19 10:55:51 +0900

    feat: 회원 생성 api 개발

--- src/domain/user/user.controller.ts
+++ src/domain/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateUserRequest } from './dto/create-user.request.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post()
+  createUser(createUser: CreateUserRequest): Promise<number> {
+    return this.userService.createUser(createUser);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: number) {}
+}
--- src/domain/user/user.service.ts
+++ src/domain/user/user.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { DataSource, EntityManager } from 'typeorm';
+import { CreateUserRequest } from './dto/create-user.request.dto';
+import { User } from './entity/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private dataSource: DataSource) {}
+
+  public async createUser(createUser: CreateUserRequest): Promise<number> {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    const entityManager: EntityManager = await queryRunner.manager;
+
+    const { name } = createUser;
+    const user: User = await entityManager.findOneBy(User, {
+      nickName: name,
+    });
+    if (user) {
+      throw new HttpException('이미 존재하는 회원입니다.', HttpStatus.CONFLICT);
+    }
+
+    await queryRunner.startTransaction();
+    try {
+      const saveUser = await entityManager.save(User, { nickName: name });
+      await queryRunner.commitTransaction();
+      return saveUser.id;
+    } catch (e) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
+  }
+}
