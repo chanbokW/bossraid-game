7d23686a41882c3007ddcfbb9504cff232cb0c3c
chanbokW <cksqhr4961@naver.com>
2022-09-19 11:03:12 +0900

    feat: bossRaid 모듈생성(controller,service)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmConfig } from './global/db/typeorm.config';
 import { UserModule } from './domain/user/user.module';
+import { BossRaidModule } from './domain/boss-raid/boss-raid.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { UserModule } from './domain/user/user.module';
     }),
     TypeOrmConfig,
     UserModule,
+    BossRaidModule,
   ],
   controllers: [],
   providers: [],
