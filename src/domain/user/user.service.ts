e3b33447300e69b7d66531233135be9deba8c7ee
chanbokW <cksqhr4961@naver.com>
2022-09-21 19:20:43 +0900

    refactor: 회원조회시 보스 리턴값변경

--- src/domain/user/user.service.ts
+++ src/domain/user/user.service.ts
@@ -1,5 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { DataSource, EntityManager } from 'typeorm';
+import { BossRaidRecord } from '../boss-raid/entity/bossRaid.entity';
+import { BossRaidHistory } from './dto/bossRaid.history';
 import { CreateUserRequest } from './dto/create-user.request.dto';
 import { User } from './entity/user.entity';
 
@@ -36,11 +38,26 @@ export class UserService {
     const queryRunner = this.dataSource.createQueryRunner();
 
     // Todo 응답할 데이터 변경 (변수명 )
-    return await queryRunner.manager.findOne(User, {
+    const user: User = await queryRunner.manager.findOne(User, {
       where: { id },
       relations: {
         raidHistory: true,
       },
     });
+
+    const bossRaids: BossRaidRecord[] = user.raidHistory;
+
+    let totalScore = 0;
+    if (bossRaids) {
+      bossRaids.map((e) => {
+        totalScore += e.score;
+        return new BossRaidHistory(e.id, e.score, e.startTime, e.endTime);
+      });
+    }
+
+    return {
+      totalScore,
+      bossRaids,
+    };
   }
 }
